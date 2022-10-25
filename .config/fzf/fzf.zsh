1cdc3abff26ffac6556cf8268d5fcea1cfbb9b06
captainko <thongnguyen.uit@gmail.com>
2022-10-25 13:51:34 +0700

    update

---
+++ .config/fzf/fzf.zsh
@@ -0,0 +1,13 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/cko/.local/src/fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/home/cko/.local/src/fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/home/cko/.local/src/fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+# source "/home/cko/.local/src/fzf/shell/key-bindings.zsh"
