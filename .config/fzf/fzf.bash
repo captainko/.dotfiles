2d5b6af61297be29bb9c295d9f73f675bd75fa1e
captainko <thongnguyen.uit@gmail.com>
2022-12-07 12:36:08 +0700

    feat(fzf): update `fzf` path

--- .config/fzf/fzf.bash
+++ .config/fzf/fzf.bash
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/cko/.local/src/fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/home/cko/.local/src/fzf/bin"
+if [[ ! "$PATH" == */home/cko/drives/dev/contributes/fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/home/cko/drives/dev/contributes/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/cko/.local/src/fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "/home/cko/drives/dev/contributes/fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/cko/.local/src/fzf/shell/key-bindings.bash"
+source "/home/cko/drives/dev/contributes/fzf/shell/key-bindings.bash"
