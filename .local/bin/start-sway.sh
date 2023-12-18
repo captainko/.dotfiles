63483846d13cd39b443f500745c921845c9f36fe
captainko <thongnguyen.uit@gmail.com>
2023-12-18 21:48:39 +0700

    feat(sway): update wayland config

--- .local/bin/start-sway.sh
+++ .local/bin/start-sway.sh
@@ -2,6 +2,11 @@
 eval "$(/usr/bin/gnome-keyring-daemon --start)"
 export SSH_AUTH_SOCK
 
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_STATE_HOME="$HOME/.local/state"
+
 # flameshot
 export SDL_VIDEODRIVER=wayland
 export _JAVA_AWT_WM_NONREPARENTING=1
